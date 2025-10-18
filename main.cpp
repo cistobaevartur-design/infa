e1989afcdd7da22ac8c9682c7d0094977f5a02e7
Чистобаев Артур Александрович и ИП-516 <chistoart.23@gmail.com>
2025-10-18 10:14:47 +0700

    Исправлено приветствие

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 
 int main(int argc, char** argv) {
     char* str = (char*)malloc(sizeof(char) * SIZE);
-    printf("Введите своё имя: \n");
-    scanf("%s", str);
+    printf("Введите своё имя:\n");
+    str = fgets(str, SIZE, stdin);
     puts("Привет");
     puts(str);
     return 0;
